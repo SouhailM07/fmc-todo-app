8067a0d5a19b144768870731f118166fbe56484a
souhail <lightshadow416@gmail.com>
2024-02-11 17:19:31 +0100

    adding some style and completed the crud operators

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,34 @@
+"use client";
+import useSWR from "swr";
+import axios from "axios";
+import { useState } from "react";
 import "./input.css";
 // shadcn
 export default function MyInput() {
+  let [createdNote, setCreatedNote] = useState<string>("");
   return (
     <>
-      <div className="flex items-center bg-gray-500  px-[1rem]">
-        <div className="border-2 border-red-500 h-[1rem] w-[1rem] rounded-full"></div>
-        <input
-          type="text"
-          placeholder="Create a new todo..."
-          className="!outline-none py-[0.4rem] indent-[1rem] bg-gray-500 w-full"
-        />
+      <div className="flex items-center bg-very-light-gray text-very-dark-grayish-blue px-[1rem]">
+        <div className="border border-dark-grayish-blue h-[1rem] w-[1rem] rounded-full"></div>
+        <form
+          className=" w-full"
+          onSubmit={async (e) => {
+            if (createdNote.length < 1) {
+              e.preventDefault();
+            }
+            await axios.post("/api/notes", {
+              note: createdNote,
+            });
+          }}
+        >
+          <input
+            value={createdNote}
+            onChange={(e) => setCreatedNote(e.target.value)}
+            type="text"
+            placeholder="Create a new todo..."
+            className="!outline-none py-[0.5rem] indent-[1rem] bg-transparent w-full "
+          />
+        </form>
       </div>
     </>
   );
--- src/components/NotesContainer/NotesContainer.tsx
+++ src/components/NotesContainer/NotesContainer.tsx
@@ -10,39 +10,38 @@ import { InfoPanel } from "@/components";
 //
 import axios from "axios";
 import useSWR from "swr";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 // assets
 import Image from "next/image";
 
 export default function NotesContainer() {
-  let [notes, setNotes]: any = useState([""]);
-  let [skeleton, setSkeletons] = useState<boolean>(false);
-  let getData = () => {
-    // setSkeletons(true);
-    axios.get("/api/notes").then((data) => {
-      // setSkeletons(false);
-      setNotes(data.data.notes);
-    });
-  };
+  // * api [start]
   // @ts-ignore
   let fetcher = (...args) => axios.get(...args);
   let { data, error, isLoading, mutate } = useSWR("/api/notes", fetcher);
-  if (isLoading) console.log("loading");
+  if (error) console.log(error);
+  // * api [end]
 
   useEffect(() => {
     console.log("updating");
   }, []);
   return (
     <>
-      <article className="w-full border-2 border-green-500">
+      <article className="w-full bg-very-light-gray rounded-md">
         <section>
           <ScrollArea className="h-[10rem]">
             <ul className="min-h-[8rem] ">
-              {skeleton ? (
+              {isLoading ? (
                 <>
-                  <Skeleton className="w-full h-[3rem]" />
-                  <Skeleton className="w-full h-[3rem]" />
-                  <Skeleton className="w-full h-[3rem]" />
+                  <Skeleton className="w-full h-[3rem] grid place-items-center">
+                    loading
+                  </Skeleton>
+                  <Skeleton className="w-full h-[3rem] grid place-items-center">
+                    loading
+                  </Skeleton>
+                  <Skeleton className="w-full h-[3rem] grid place-items-center">
+                    loading
+                  </Skeleton>
                 </>
               ) : (
                 data?.data.notes.map((e, i) => {
@@ -69,14 +68,12 @@ export default function NotesContainer() {
 let Note = ({ note, done, noteId, f }) => {
   return (
     <>
-      <li className="note px-[1rem] flex py-[0.6rem] bg-gray-400 items-center justify-between">
+      <li className="note px-[1rem] flex py-[0.6rem] border-y-very-light-grayish-blue border text-very-dark-grayish-blue bg-transparent items-center justify-between">
         <div
-          onClick={() => {
-            noteState(noteId).then(async () => {
-              await axios.put(`/api/notes?id=${noteId}`);
-              await f();
-              console.log("check render");
-            });
+          onClick={async () => {
+            await axios.put(`/api/notes?id=${noteId}`);
+            await f();
+            console.log("check render");
           }}
           className="items-center flex"
         >
@@ -84,7 +81,7 @@ let Note = ({ note, done, noteId, f }) => {
             className={`${
               done &&
               "bg-gradient-to-r from-check-background-start to-check-background-end"
-            } border h-[1rem] bg-no-repeat bg-center grid place-items-center w-[1rem] rounded-full`}
+            } border border-dark-grayish-blue h-[1rem] bg-no-repeat bg-center grid place-items-center w-[1rem] rounded-full`}
           >
             <Image
               src="icon-check.svg"
@@ -97,6 +94,11 @@ let Note = ({ note, done, noteId, f }) => {
           <p className="indent-[1rem]">{note}</p>
         </div>
         <Image
+          onClick={async () => {
+            await axios.delete(`/api/notes?id=${noteId}`);
+            await f();
+            console.log("check delete render");
+          }}
           width={14}
           height={14}
           src="icon-cross.svg"
